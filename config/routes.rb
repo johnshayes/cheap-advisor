440742d88b5aae70298288eb100fb2d32549248d
John Hayes <john@ontheinternet.org.uk>
2017-08-27 12:52:31 +0200

    restaurants, controllers & routing with Rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :restaurants
+
+  # # read all restaurants
+  # get "restaurants", to: "restaurants#index"
+
+  # # read one restaurant
+  # get "restaurants/:id", to: "restaurants#show"
+
+  # # create one restaurant
+  # get "restaurants/new", to: "restautants#new" # To get form
+  # post "restaurants", to: "restaurants#create" # To create
+
+  # # update one restaurant
+  # get "restaurants/:id/edit", to: "restaurants#edit"
+  # patch "restaurants/:id", to: "restaurants#update"
+
+  # # delete one restaurant
+  # delete "restaurants/:id", to: "restaurants#destroy"
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: "Dishoom", address: "shoreditch", rating: 5)
+Restaurant.create(name: "Itsu", address: "city", rating: 2)
+Restaurant.create(name: "Horse & Groom", address: "city", rating: 3)
